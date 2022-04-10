82ce2e73f7e849ebce7b41118caf289fe8b8a55c
Kévin Gatel <kevin.gatel@insa'rouen.fr>
2022-04-10 22:27:24 +0200

    Initialisation des fichiers .cpp et .hpp

---
+++ Codeur.hpp
@@ -0,0 +1,46 @@
+#ifndef CODEUR_H
+#define CODEUR_H
+
+#include <string>
+
+#include "Joueur.hpp"
+
+using std::string;
+using namespace std;
+
+/*! \class Codeur
+* \brief Joueur proposant la combinaison recherchée
+* \author Groupe A7
+* \version 1.0
+* \date avril 2022
+*
+* Cette classe hérite de la classe Joueur, elle représente l'humain ou l'ordi/IA qui devra proposer une combinaison à trouver pour le joueur décodeur. Elle possède les mêmes attributs que Joueur avec un argument en plus, le mot de passe qui sera demandé au moment de voir la combinaison cherchée s'il le souhaite durant une partie.
+*/
+
+class Codeur {
+	private:
+		string motDePasse;
+	
+	public:
+		/*! \fn Codeur(string mdp)
+		  * \brief Constructeur de la classe Codeur
+		  * \param mdp le mot de passe du joueur codeur
+		  */
+		Codeur(string mdp);
+		
+		/*! \fn setMdp(string mdp);
+		* \brief setteur de l'attribut motDePasse
+		* \param mdp string 
+		*/
+		void setMdp(string mdp);
+		
+		/*! \fn getMdp()
+		* \brief getteur de l'attribut motDePasse
+		* \return une chaine de caractère
+		*/
+		string getMdp();
+		
+};
+
+#endif // CODEUR_H
+
---
+++ Combinaison.hpp
@@ -0,0 +1,73 @@
+#ifndef COMBINAISON_H
+#define COMBINAISON_H
+
+#include <string>
+#include <iostream>
+
+
+using std::string;
+using namespace std;
+
+/*! \class Combinaison
+* \brief Tableau d'entier représentant une combinaison de couleur ou de lettre
+* \author Groupe A7
+* \version 1.0
+* \date avril 2022
+*
+* Cette classe permet de définir l'objet Combinaison. Elle transforme une chaine de caractère (suite de couleur ou de lettre) séparée d'espace en tableau d'entier. Elle possède un seul attribut : le tableau d'entier.
+*/
+
+class Combinaison {
+	private:
+		int [] combinaison;
+	
+	public:
+		/*! \fn Combinaison(string chaine)
+		  * \brief Constructeur de la classe Combinaison
+		  * \param chaine est la chaine de caractère comprenant la suite de couleur ou de lettre séparée d'espace
+		  */
+		Combinaison(string chaine);
+		
+		/*! \fn setCombinaison(string chaine);
+		* \brief setteur de l'attribut combinaison
+		* \param chaine string 
+		*/
+		void setCombinaison(string chaine);
+		
+		/*! \fn get()
+		* \brief getteur de l'attribut combinaison
+		* \return un tableau d'entier
+		*/
+		int [] get();
+		
+		/*! \fn get(int i)
+		* \brief getteur du ième élément de l'attribut combinaison
+		* \param i entier : l'indice
+		* \return un entier
+		*/
+		int get(int i);
+		
+		/*! \fn int bienPlace(Combinaison code)
+		* \brief renvoie le nombre d'élément (couleur ou lettre) bien placé par rapport à la combinaison du codeur
+		* \param code Combinaison : la combinaison valide recherchée par le décodeur
+		* \return un entier
+		*/
+		int bienPlace(Combinaison code);
+		
+		/*! \fn int malPlace(Combinaison code)
+		* \brief renvoie le nombre d'élément (couleur ou lettre) appartenant à la combinaison du codeur mais mal placé
+		* \param code Combinaison : la combinaison valide recherchée par le décodeur
+		* \return un entier
+		*/
+		int malPlace(Combinaison code);
+		
+		/*! \fn string toString();
+		* \brief permet l'affichage de la combinaison
+		* \return un string
+		*/
+		string toString();
+		
+};
+
+#endif // CODEUR_H
+
