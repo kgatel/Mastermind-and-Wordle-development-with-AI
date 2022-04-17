91c912242b66ef8f74718145adc7d9737c366b9d
Bennani Ghali <ghali.bennani@insa-rouen.fr>
2022-04-17 20:24:22 +0000

    Ajout IA_Codeur_Wordle.hpp

---
+++ IA_Codeur_Wordle.hpp
@@ -0,0 +1,36 @@
+#ifndef IA_Codeur_Wordle_H
+#define IA_Codeur_Wordle_H
+#include <string>
+
+#include "Combinaison.hpp"
+#include "IA_Codeur.hpp"
+
+
+/*! \class IA_Codeur_Wordle
+* \brief Cette classe est utilisée pour représenter un joueur virtuel (IA) en mode codeur dans le mode de jeu Wordle
+* \author Groupe A7
+* \version 1.0
+* \date avril 2022
+*
+*/
+
+class IA_Codeur_Wordle : public IA_Codeur{
+
+	
+		/*! \fn IA_Codeur_Wordle()
+		  * \brief Constructeur de la classe IA_Codeur_Wordle
+		  */
+		IA_Codeur_Wordle();
+		
+		/*! \fn Combinaison genererCode()
+		* \brief méthode permettant à la machine de générer un code que le joueur artificiel décodeur devra ensuite trouver
+		*\return retourne le code créé par la machine
+		*/
+		Combinaison genererCode();
+		  
+		
+		  
+	
+};
+
+#endif
