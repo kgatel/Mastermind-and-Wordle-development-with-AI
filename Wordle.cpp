54816288c4b047640668984b2834dc7cd65aca4e
Boeno Thomas <tboeno@asi-testing>
2022-04-29 14:42:29 +0200

    afficher partie wordle

--- Wordle.cpp
+++ Wordle.cpp
@@ -1,5 +1,7 @@
 #include <string>
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <vector>
 #include "Wordle.hpp"
 #include "JeuDeDeduction.hpp"
@@ -27,7 +29,7 @@ Wordle :: Wordle(Joueur *codeur1, Joueur *decodeur1) : JeuDeDeduction(codeur1,de
 void Wordle :: partie(){
 	FonctionsUtiles f;
 	f.clear();
-	cout << f.vert("Bienvenue dans le Wordle !\n") << endl;
+	cout << f.vertclair("Bienvenue dans le Wordle !\n") << endl;
 	(*codeur).jouer();
 	do{
 		cout << f.jaune("\nTour n°"+f.intToString(numeroTour+1))<<endl;
@@ -47,7 +49,44 @@ void Wordle :: partie(){
 }
 
 void Wordle :: afficherHistorique(){
-
+	FonctionsUtiles f;
+	string resultat = CombiWordle((*decodeur).getCombinaison()).resultat((*codeur).getCombinaison());
+	int i,j;
+	int taille;
+	taille=(int)historiqueCombinaison.size();
+	string espace="";
+	string underscore="";
+	for (int jj=1;jj<Menu::NB_CASE;jj++){
+		underscore+="_______";
+	}
+	cout <<"________________"+underscore+"___"+f.cyan("Résultats")<< endl;
+	for (i=0;i<Menu::NB_TOUR-taille ; i++) {
+		if (Menu::NB_TOUR-i<10) {
+			espace=" ";
+		}else{
+			espace="";
+		}
+		string chaine="";
+		for (int cpt=0;cpt<Menu::NB_CASE;cpt++){
+			chaine+=f.carrevide()+"      ";
+		}
+		if ((i==Menu::NB_TOUR-taille-1)&&(detectionVictoire()!=decodeur)){
+			cout<<f.cyan("Tentative : ")<<espace<<Menu::NB_TOUR-i<<"  "+f.blink(chaine)+"     |"<<endl;
+		}else{
+			cout<<f.cyan("Tentative : ")<<espace<<Menu::NB_TOUR-i<<"  "+chaine+"     |"<<endl;
+		}
+	}
+	for (j=0;j<taille ; j++) {
+		if (taille-j<10) {
+			espace=" ";
+		}else{
+			espace="";
+		}
+		cout<<f.cyan("Tentative : ")<<espace<<taille-j<<" ";
+		cout << CombiWordle(historiqueCombinaison[taille-j-1]).toString();
+		cout<<" --> "+historiqueResultat[taille-j-1]<< endl;
+	}
+	cout <<"________________"+underscore+"____________|"<< endl;
 }
 
 
